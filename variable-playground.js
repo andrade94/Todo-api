9eb9b510dbb969776b0d4abc9354c76c2a158045
andrade94 <eduardo.mtz9@gmail.com>
2017-01-28 19:17:46 -0600

    variable playground

---
+++ variable-playground.js
@@ -0,0 +1,25 @@
+// var person = {
+
+// 	name: 'Eduardo',
+// 	age: 21
+// };
+
+// function updatePerson (obj) {
+// 	obj = {
+// 		name: 'Eduardo',
+// 		age: 22
+// 	};
+// };
+
+// updatePerson(person);
+// console.log(person);
+
+var grades = [15, 88];
+
+function addGrade (grades) {
+	grades.push(14);
+	debugger;
+};
+
+addGrade(grades);
+console.log(grades);
